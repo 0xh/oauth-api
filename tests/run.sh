fb2cf8549b5e27c0d2f383938a783a9d5918919f
Miroslav Čillík <miro@keboola.com>
2017-11-22 16:30:22 +0100

    :cop: travis has problem with dynamo local install

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-serverless dynamodb install \
+serverless dynamodb remove \
+&& serverless dynamodb install \
 && ./node_modules/.bin/mocha --timeout 0 --compilers js:babel-core/register tests/app
