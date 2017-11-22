1bd29e3caec8015c197985eced39be276be6b495
Miroslav Čillík <miro@keboola.com>
2017-11-22 16:38:53 +0100

    :cop: supress the stdout of dynamo db install as it messes up log in travis

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 serverless dynamodb remove \
-&& serverless dynamodb install \
+&& serverless dynamodb install >/dev/null \
 && ./node_modules/.bin/mocha --timeout 0 --compilers js:babel-core/register tests/app
